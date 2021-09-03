28b7908e1df428bd3b9079cb4b15105580506481
Bradley Roybal <broybal95@gmail.com>
2021-09-03 16:58:22 -0500

    changed templates

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
